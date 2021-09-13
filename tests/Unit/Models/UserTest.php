1d7a9a0d08494746db7c47366d689c24dc829de8
Nasyrov Evgenii <inasyrov@ya.ru>
2021-09-14 00:01:01 +0100

    Test user model

---
+++ tests/Unit/Models/UserTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Concerns\UnguardsAttributes;
+use App\Models\Diary;
+use App\Models\Group;
+use App\Models\Message;
+use App\Models\Plan;
+use App\Models\Project;
+use App\Models\QueryBuilders\UserQueryBuilder;
+use App\Models\Ticket;
+use App\Models\User;
+use App\Models\Wage;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /** @test */
+    public function it_extends_authenticable_class(): void
+    {
+        $user = new User();
+
+        $this->assertInstanceOf(Authenticatable::class, $user);
+    }
+
+    /** @test */
+    public function it_uses_has_api_tokens_concern(): void
+    {
+        $this->assertArrayHasKey(HasApiTokens::class, class_uses(User::class));
+    }
+
+    /** @test */
+    public function it_uses_notifiable_concern(): void
+    {
+        $this->assertArrayHasKey(Notifiable::class, class_uses(User::class));
+    }
+
+    /** @test */
+    public function it_uses_unguard_attributes_concern(): void
+    {
+        $this->assertArrayHasKey(UnguardsAttributes::class, class_uses(User::class));
+    }
+
+    /** @test */
+    public function it_hides_password_attribute(): void
+    {
+        $user = new User();
+
+        $this->assertTrue(in_array('password', $user->getHidden(), true));
+    }
+
+    /** @test */
+    public function it_hides_remember_token_attribute(): void
+    {
+        $user = new User();
+
+        $this->assertTrue(in_array('remember_token', $user->getHidden(), true));
+    }
+
+    /** @test */
+    public function it_casts_email_verified_at_attribute_to_datetime(): void
+    {
+        $user = new User();
+
+        $this->assertTrue($user->hasCast('email_verified_at', 'datetime'));
+    }
+}
