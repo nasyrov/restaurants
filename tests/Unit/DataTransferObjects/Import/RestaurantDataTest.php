d568dae65639c5bdd34fb9b0db2498e7d08bee5d
Nasyrov Evgenii <inasyrov@ya.ru>
2021-09-15 00:21:05 +0100

    Fix restaurant data test

--- tests/Unit/DataTransferObjects/Import/RestaurantDataTest.php
+++ tests/Unit/DataTransferObjects/Import/RestaurantDataTest.php
@@ -12,6 +12,9 @@ class RestaurantDataTest extends TestCase
     {
         $data = RestaurantData::fromRecordWithHeader([
             'Restaurant name' => $name = $this->faker->company,
+            'Opens'           => $this->faker->time,
+            'Closes'          => $this->faker->time,
+            'Days Open'       => 'Mo,Tu',
         ]);
 
         $this->assertInstanceOf(RestaurantData::class, $data);
@@ -24,6 +27,7 @@ class RestaurantDataTest extends TestCase
     {
         $data = RestaurantData::fromRecordWithoutHeader([
             0 => $name = $this->faker->company,
+            1 => 'Mon-Thu, Sun 11:30 am - 9 pm  / Fri-Sat 11:30 am - 10 pm',
         ]);
 
         $this->assertInstanceOf(RestaurantData::class, $data);
