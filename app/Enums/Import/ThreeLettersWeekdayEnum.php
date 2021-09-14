bfab72830fa83483c50f002f04df3872b1803ddd
Nasyrov Evgenii <inasyrov@ya.ru>
2021-09-14 02:48:20 +0100

    Scaffold three letters weekday enum

---
+++ app/Enums/Import/ThreeLettersWeekdayEnum.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Enums\Import;
+
+use Spatie\Enum\Laravel\Enum;
+
+/**
+ * @method static self Mon()
+ * @method static self Tue()
+ * @method static self Wed()
+ * @method static self Thu()
+ * @method static self Fri()
+ * @method static self Sat()
+ * @method static self Sun()
+ */
+final class ThreeLettersWeekdayEnum extends Enum
+{
+    protected static function values(): array
+    {
+        return [
+            'Mon' => 1,
+            'Tue' => 2,
+            'Wed' => 3,
+            'Thu' => 4,
+            'Fri' => 5,
+            'Sat' => 6,
+            'Sun' => 7,
+        ];
+    }
+}
