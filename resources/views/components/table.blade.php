0126464f0e0d90107fc8b6c8663f58386455e0bd
Nasyrov Evgenii <inasyrov@ya.ru>
2021-09-15 03:38:20 +0100

    Scaffold table component

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,21 @@
+<div {{ $attributes->merge(['class' => 'flex flex-col'])->only(['class', 'wire:loading.class']) }}>
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                    <tr>
+                        {{ $head }}
+                    </tr>
+                    </thead>
+
+                    <tbody class="bg-white divide-y divide-gray-200">
+                    {{ $slot }}
+                    </tbody>
+                </table>
+
+                {{ $pagination ?? '' }}
+            </div>
+        </div>
+    </div>
+</div>
