a75bd5734a37bc9ccaa8a7547d86c474a2cd7739
Nasyrov Evgenii <inasyrov@ya.ru>
2021-09-15 03:46:56 +0100

    Test home page contains livewire component

--- tests/Feature/Web/HomePageTest.php
+++ tests/Feature/Web/HomePageTest.php
@@ -2,17 +2,19 @@
 
 namespace Tests\Feature\Web;
 
+use App\Http\Livewire\Restaurants;
 use Tests\TestCase;
 
 class HomePageTest extends TestCase
 {
     /** @test */
-    public function it_shows_home_page(): void
+    public function it_shows_home_page_that_contains_livewire_component(): void
     {
         $this
             ->withoutMix()
             ->get(route('home'))
             ->assertOk()
-            ->assertViewIs('home');
+            ->assertViewIs('home')
+            ->assertSeeLivewire(Restaurants::class);
     }
 }
