a1e9d98d7981e463b0c851bf26fc603b2e8d64ec
Nasyrov Evgenii <inasyrov@ya.ru>
2021-09-15 11:50:00 +0100

    Test it displays currently open restaurants

--- tests/Feature/Web/RestaurantsTest.php
+++ tests/Feature/Web/RestaurantsTest.php
@@ -4,21 +4,43 @@ namespace Tests\Feature\Web;
 
 use App\Http\Livewire\Restaurants;
 use App\Models\Restaurant;
+use App\Models\Schedule;
+use Carbon\Carbon;
 use Livewire\Livewire;
 use Tests\TestCase;
 
 class RestaurantsTest extends TestCase
 {
     /** @test */
-    public function it_shows_restaurants(): void
+    public function it_shows_currently_open_restaurants(): void
     {
-        $restaurants = Restaurant::factory()
-            ->count(2)
+        Carbon::setTestNow(Carbon::parse('14-09-2021 10:05:00'));
+
+        $restaurant1 = Restaurant::factory()
             ->create();
+
+        $schedule1 = Schedule::factory()
+            ->create([
+                'restaurant_id' => $restaurant1->id,
+                'weekday'       => 2,
+                'open'          => '10:00:00',
+                'close'         => '11:00:00',
+            ]);
+
+        $restaurant2 = Restaurant::factory()
+            ->create();
+
+        $schedule2 = Schedule::factory()
+            ->create([
+                'restaurant_id' => $restaurant2->id,
+                'weekday'       => 3,
+                'open'          => '10:00:00',
+                'close'         => '11:00:00',
+            ]);
 
         Livewire::test(Restaurants::class)
             ->assertViewIs('livewire.restaurants')
-            ->assertSee($restaurants->get(0)->name)
-            ->assertSee($restaurants->get(1)->name);
+            ->assertSee($restaurant1->name)
+            ->assertDontSee($restaurant2->name);
     }
 }
