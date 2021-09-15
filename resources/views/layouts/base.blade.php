f599b860da78dc5704cba5ea425bf4b1ea9d6f62
Evgenii Nasyrov <inasyrov@yandex.ru>
2021-09-15 03:40:39 +0100

    Blade components (#17)
    * Install blade-ui-kit/blade-icons composer package
    
    * Configure blade icons
    
    * Install blade-ui-kit/blade-heroicons composer package
    
    * WIP layout
    
    * Scaffold table component

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,7 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include('layouts.partials.head')
 
-    <body class="antialiased text-gray-900">
-        @yield('body')
+    <body class="font-sans py-6 bg-gray-100 antialiased">
+        <main class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @yield('body')
+        </main>
     </body>
 </html>
