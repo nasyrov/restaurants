862bb50fc2c375421ecaa4d133b1743bf27f547d
Nasyrov Evgenii <inasyrov@ya.ru>
2021-09-15 03:56:53 +0100

    Display restaurants

--- app/Http/Livewire/Restaurants.php
+++ app/Http/Livewire/Restaurants.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Restaurant;
 use Illuminate\Contracts\View\View as ViewContract;
 use Illuminate\Support\Facades\View;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Restaurants extends Component
 {
+    use WithPagination;
+
     public function render(): ViewContract
     {
-        return View::make('livewire.restaurants');
+        return View::make('livewire.restaurants', [
+            'restaurants' => Restaurant::query()
+                ->paginate(),
+        ]);
     }
 }
