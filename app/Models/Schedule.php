f1846efb88c17f3276b99e510c76b3aea5ae8d82
Nasyrov Evgenii <inasyrov@ya.ru>
2021-09-15 11:40:16 +0100

    Add schedule's helpers to determine whether we are within working hours

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Concerns\UnguardsAttributes;
 use App\Models\QueryBuilders\ScheduleQueryBuilder;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -21,6 +22,23 @@ class Schedule extends Model
     public function newEloquentBuilder($query): ScheduleQueryBuilder
     {
         return new ScheduleQueryBuilder($query);
+    }
+
+    public function isOpen(): bool
+    {
+        $open  = Carbon::createFromFormat('H:i:s', $this->open);
+        $close = Carbon::createFromFormat('H:i:s', $this->close);
+
+        if ($close->lessThan($open)) {
+            $close->addDay();
+        }
+
+        return Carbon::now()->isBetween($open, $close);
+    }
+
+    public function isClosed(): bool
+    {
+        return !$this->isOpen();
     }
 
     public function restaurant(): BelongsTo
